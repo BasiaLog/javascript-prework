70b572127decf9ad04bb56edec936606b7b3beaf
Barbara Pisarek <barbara.pisarek12@gmail.com>
2020-01-28 19:16:13 +0100

    Add 'let' and 'const'

--- js/script.js
+++ js/script.js
@@ -1,22 +1,17 @@
-var argButtonName, buttonRock, buttonPaper, buttonScissors;
-
-
-buttonRock = document.getElementById('button-rock');
-buttonPaper = document.getElementById('button-paper');
-buttonScissors = document.getElementById('button-scissors');
+const buttonRock = document.getElementById('button-rock');
+const buttonPaper = document.getElementById('button-paper');
+const buttonScissors = document.getElementById('button-scissors');
 
 function buttonClicked(argButtonName) {
   clearMessages();
   console.log(argButtonName + ' został kliknięty');
-  playerMove = argButtonName;
-  randomNumber = Math.floor(Math.random() * 3 + 1);
+  let playerMove = argButtonName;
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
   console.log('wylosowana liczba to: ' + randomNumber);
-  computerMove = getMoveName(randomNumber);
+  let computerMove = getMoveName(randomNumber);
   console.log('ruch komputera to: ' + computerMove);
   displayResult(playerMove, computerMove);
 }
-
-var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
 
 function getMoveName(argMoveId) {
   console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
@@ -45,11 +40,8 @@ function displayResult(argPlayerMove, argComputerMove) {
   } else {
     printMessage('Przegrywasz :(');
   }
-  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove + '.');
 }
-
-
-
 
 buttonRock.addEventListener('click', function(){
   buttonClicked('kamień') });
